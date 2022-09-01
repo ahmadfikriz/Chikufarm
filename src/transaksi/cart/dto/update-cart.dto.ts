cdceaef72faadfd980e29067097cbebdb4d53ba2
fikri <a.fikri.zul@gmail.com>
2022-09-01 18:28:47 +0700

    Fix Status Cart

--- src/transaksi/cart/dto/update-cart.dto.ts
+++ src/transaksi/cart/dto/update-cart.dto.ts
@@ -1,4 +1,24 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsOptional, IsNumber } from 'class-validator';
+import { StatusCart } from 'src/transaksi/entities/cart.entity';
 import { CreateCartDto } from './create-cart.dto';
 
-export class UpdateCartDto extends PartialType(CreateCartDto) {}
+export class UpdateCartDto {
+  @ApiProperty({ required: true })
+  @IsOptional()
+  @IsNumber()
+  jumlah_produk: number;
+
+  @ApiProperty({ required: true })
+  @IsOptional()
+  @IsNumber()
+  total_harga: number;
+
+  @ApiProperty({
+    type: 'enum',
+    enum: StatusCart,
+    default: StatusCart.DONE,
+  })
+  @IsOptional()
+  status: StatusCart;
+}
