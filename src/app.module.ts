b38693ba95f319f60ebc58ffb87b0c7403e6297c
fikri <a.fikri.zul@gmail.com>
2022-09-16 17:10:06 +0700

    Delete Pass

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,6 @@ import { BankModule } from './transaksi/bank/bank.module';
 import { ProdukAgenModule } from './produk/produk_agen/produk_agen.module';
 import { ProdukPusatModule } from './produk/produk_pusat/produk_pusat.module';
 import { FileModule } from './file/file.module';
-import { PasswordModule } from './user/password/password.module';
 
 @Module({
   imports: [
@@ -96,7 +95,6 @@ import { PasswordModule } from './user/password/password.module';
     BankModule,
     ProdukPusatModule,
     FileModule,
-    PasswordModule,
   ],
 })
 export class AppModule {}
