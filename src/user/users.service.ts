c61e1b299851811a2070091bf01dc1acfb8c4808
fikri <a.fikri.zul@gmail.com>
2022-09-21 19:52:17 +0700

    Find Role

--- src/user/users.service.ts
+++ src/user/users.service.ts
@@ -231,23 +231,14 @@ relations: ['role'],
 async findUser(
   options: IPaginationOptions,
   search: string,
-  // role: string,
   ): Promise<Pagination<User>> {
-    const query = this.usersRepository.createQueryBuilder('user').innerJoinAndSelect('user.role', 'nama')
-
-    // const roleName = ['admin', 'agen', 'pembeli']
-
-    // if (roleName.includes(role)){
-    //   query
-    //     .where('role.nama LIKE :role', {role: `%${role}%`})
-    //     .andWhere('user.nama LIKE :search', {search: `%${search}%`})
-    //     .andWhere('user.email LIKE :search', {search: `%${search}%`})
-    // }
+    const query = this.usersRepository.createQueryBuilder('user').innerJoinAndSelect('user.role', 'role')
 
     if(search)(
       query
         .where('user.nama LIKE :search', {search: `%${search}%`})
         .orWhere('user.email LIKE :search', {search: `%${search}%`})
+        .orWhere('role.nama LIKE :search', {search: `%${search}%`})
     )
 
     else(
